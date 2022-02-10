597327e7407c5a17bf5d864bc49b18e68891da4d
Tobias Persson <tobiaspn@axis.com>
2022-02-10 09:07:00 +0100

    Cleanup subsuite endpoint (#47)
    * Decouple the sub suite endpoint for clarity
    
    This is done in preparation for the external provider
    change that is on its way.
    By making the endpoints cleaner, we can make the external
    provider changes easier.

--- src/environment_provider/webserver.py
+++ src/environment_provider/webserver.py
@@ -16,7 +16,6 @@
 """ETOS Environment Provider webserver module."""
 import os
 import logging
-import json
 import falcon
 
 from etos_lib.etos import ETOS
@@ -48,6 +47,7 @@ from environment_provider.backend.configure import (
     get_iut_provider_id,
     get_log_area_provider_id,
 )
+from environment_provider.backend.subsuite import get_sub_suite, get_id
 from environment_provider.backend.common import get_suite_id
 
 
@@ -255,8 +255,7 @@ class SubSuite:  # pylint:disable=too-few-public-methods
         """
         self.database = database
 
-    @staticmethod
-    def on_get(request, response):
+    def on_get(self, request, response):
         """Get a generated sub suite from environment provider.
 
         :param request: Falcon request object.
@@ -264,21 +263,14 @@ class SubSuite:  # pylint:disable=too-few-public-methods
         :param response: Falcon response object.
         :type response: :obj:`falcon.response`
         """
-        sub_suite_id = request.get_param("id")
-        if sub_suite_id is None:
-            raise falcon.HTTPBadRequest(
-                "Missing parameter", "'id' is a required parameter."
-            )
-        database = Database()
-        sub_suite = database.read(sub_suite_id)
-        if sub_suite:
-            response.status = falcon.HTTP_200
-            response.media = json.loads(sub_suite)
-        else:
+        suite = get_sub_suite(self.database(), get_id(request))
+        if suite is None:
             raise falcon.HTTPNotFound(
                 title="Sub suite not found.",
-                description=f"Could not find sub suite with ID {sub_suite_id}",
+                description=f"Could not find sub suite with ID {get_suite_id(request)}",
             )
+        response.status = falcon.HTTP_200
+        response.media = suite
 
 
 FALCON_APP = falcon.API(middleware=[RequireJSON(), JSONTranslator()])
