d47645965f9eb3a20fd4e2be88a1f62035a9642a
Tobias Persson <tobiaspn@axis.com>
2024-03-11 12:30:22 +0100

    Update workflow (#93)
    * Update workflow so that it builds python packages
    
    * Remove dockerfiles and webserver files

--- src/entry_debug.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-exec gunicorn environment_provider_api.webserver:FALCON_APP \
-	--name environment_provider \
-	--worker-class=gevent \
-	--bind 0.0.0.0:8001 \
-	--worker-connections=1000 \
-	--workers=5 \
-	--reload
--- src/worker.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-exec celery -A environment_provider.environment_provider.APP worker $CELERY_CMD_ARGS
